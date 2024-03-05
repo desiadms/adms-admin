846bb0bb7b1920d808c01d3be28e912ffd5f0d4b
Savannah Manning <savannahmanning@lt-tw01149.lan>
2024-03-04 22:07:23 -0700

    add grid

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,9 +1,34 @@
 import { useSignOut } from "@nhost/react";
 import { Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import { AgGridReact } from 'ag-grid-react'; // AG Grid Component
+import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the grid
+import "ag-grid-community/styles/ag-theme-quartz.css"; // Optional Theme applied to the grid
+import { useState } from "react";
 
 export function Home() {
-  return <div>hello!!!</div>;
+  const [rowData, setRowData] = useState([
+    { make: "Tesla", model: "Model Y", price: 64950, electric: true },
+    { make: "Ford", model: "F-Series", price: 33850, electric: false },
+    { make: "Toyota", model: "Corolla", price: 29600, electric: false },
+  ]);
+  
+  // Column Definitions: Defines the columns to be displayed.
+  const colDefs = [
+    { field: "make" },
+    { field: "model" },
+    { field: "price" },
+    { field: "electric" }
+  ];
+  return   <div
+  className="ag-theme-quartz" // applying the grid theme
+  style={{ height: 500 }} // the grid will fill the size of the parent container
+ >
+   <AgGridReact
+       rowData={rowData}
+       columnDefs={colDefs}
+   />
+ </div>
 }
 
 export function Dashboard() {
