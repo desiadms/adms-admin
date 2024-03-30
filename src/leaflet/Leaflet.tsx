ba5bba007aff23d82211b0f6b0ce7cd61772e06a
Lucio D'Alessandro <lda@juxt.pro>
2024-03-30 03:24:54 +0100

    Use eventually different icons per task type

--- src/leaflet/Leaflet.tsx
+++ src/leaflet/Leaflet.tsx
@@ -1,4 +1,4 @@
-import { LatLngTuple } from "leaflet";
+import { Icon, LatLngTuple } from "leaflet";
 import "leaflet/dist/leaflet.css";
 import {
   LayerGroup,
@@ -14,11 +14,22 @@ import { useTasksLatLon } from "../reports/hooks";
 const USALatLng = [37.0902, -95.712] satisfies LatLngTuple;
 const center = USALatLng;
 
-export function Leaflet({
-  groupedTasks,
-}: {
-  groupedTasks: ReturnType<typeof useTasksLatLon>["data"];
-}) {
+// create a market per grupedTask
+
+type TGroupedTasks = ReturnType<typeof useTasksLatLon>["data"];
+
+const markers = {
+  tasks_stump_removal:
+    "https://leafletjs.com/examples/custom-icons/leaf-green.png",
+  tasks_tree_removal:
+    "https://leafletjs.com/examples/custom-icons/leaf-orange.png",
+  tasks_collection: "https://leafletjs.com/examples/custom-icons/leaf-blue.png",
+  tasks_disposal: "https://leafletjs.com/examples/custom-icons/leaf-shadow.png",
+  tasks_ticketing:
+    "https://leafletjs.com/examples/custom-icons/leaf-shadow.png",
+} as const satisfies { [K in TGroupedTasks[number]["key"]]: string };
+
+export function Leaflet({ groupedTasks }: { groupedTasks: TGroupedTasks }) {
   return (
     <MapContainer
       style={{ height: `calc(100dvh - ${headerHeight}px)` }}
@@ -38,6 +49,12 @@ export function Leaflet({
                 {tasks.map((task) => {
                   return (
                     <Marker
+                      icon={
+                        new Icon({
+                          iconUrl: markers[key],
+                          iconSize: [38, 95],
+                        })
+                      }
                       key={task.id}
                       position={[task.latitude, task.longitude]}
                     >
