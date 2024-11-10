08b37bacf577088c303d41832245fb483ef1f790
Lucio D'Alessandro <lda@juxt.pro>
2024-11-10 21:53:34 +0100

    Remove leaflet thingy

--- src/reports/MapTasks.tsx
+++ src/reports/MapTasks.tsx
@@ -1,12 +1,12 @@
 import { Box, Option, Select } from "@mui/joy";
-import L, { LatLngTuple } from "leaflet";
+import L, { type LatLngTuple } from "leaflet";
 import "leaflet/dist/leaflet.css";
 import { useState } from "react";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import { DateRange } from "../components/DateRange";
 import { headerHeight, tableTopToolbarHeight } from "../globals";
 import { MapButton } from "./MapButton";
-import { useAllTasksByProject } from "./hooks";
+import type { useAllTasksByProject } from "./hooks";
 
 const position = [37.09024, -95.712891] satisfies LatLngTuple;
 
@@ -14,156 +14,152 @@ const position = [37.09024, -95.712891] satisfies LatLngTuple;
 
 type TTasks = ReturnType<typeof useAllTasksByProject>["data"];
 type TaskWithCoordinates = {
-  latitude: number;
-  longitude: number;
+	latitude: number;
+	longitude: number;
 } & TTasks[number];
 
 const Icon = new L.Icon.Default();
 Icon.options.shadowSize = [0, 0];
 
 function MapMarker({ task }: { task: TaskWithCoordinates }) {
-  return (
-    <Marker
-      icon={Icon}
-      position={[task.latitude, task.longitude] as LatLngTuple}
-    >
-      <Popup>
-        <Box>
-          <Box>{task.taskName}</Box>
-          <Box>lat: {task.latitude.toFixed(4)}</Box>
-          <Box>lon: {task.longitude.toFixed(4)}</Box>
-          <Box>by: {task.userPin?.split("@")[0]}</Box>
-        </Box>
-      </Popup>
-    </Marker>
-  );
+	return (
+		<Marker
+			icon={Icon}
+			position={[task.latitude, task.longitude] as LatLngTuple}
+		>
+			<Popup>
+				<Box>
+					<Box>{task.taskName}</Box>
+					<Box>lat: {task.latitude.toFixed(4)}</Box>
+					<Box>lon: {task.longitude.toFixed(4)}</Box>
+					<Box>by: {task.userPin?.split("@")[0]}</Box>
+				</Box>
+			</Popup>
+		</Marker>
+	);
 }
 
 function TaskTypeFilter({
-  selectedTypes,
-  setSelectedTypes,
-  taskTypes,
+	selectedTypes,
+	setSelectedTypes,
+	taskTypes,
 }: {
-  selectedTypes: string[];
-  setSelectedTypes: React.Dispatch<React.SetStateAction<string[]>>;
-  taskTypes: string[];
+	selectedTypes: string[];
+	setSelectedTypes: React.Dispatch<React.SetStateAction<string[]>>;
+	taskTypes: string[];
 }) {
-  return (
-    <Box>
-      <Select
-        multiple
-        size="sm"
-        placeholder="All task types"
-        value={selectedTypes}
-        onChange={(_event, value) => setSelectedTypes(value)}
-        renderValue={(selected) => (
-          <Box>
-            {selected.length > 1 ? (
-              <Box>{selected.length} task types</Box>
-            ) : (
-              selected.map((value) => {
-                return <Box key={value.value}>{value.value}</Box>;
-              })
-            )}
-          </Box>
-        )}
-      >
-        {taskTypes.map((type) => (
-          <Option key={type} value={type}>
-            {type}
-          </Option>
-        ))}
-      </Select>
-    </Box>
-  );
+	return (
+		<Box>
+			<Select
+				multiple
+				size="sm"
+				placeholder="All task types"
+				value={selectedTypes}
+				onChange={(_event, value) => setSelectedTypes(value)}
+				renderValue={(selected) => (
+					<Box>
+						{selected.length > 1 ? (
+							<Box>{selected.length} task types</Box>
+						) : (
+							selected.map((value) => {
+								return <Box key={value.value}>{value.value}</Box>;
+							})
+						)}
+					</Box>
+				)}
+			>
+				{taskTypes.map((type) => (
+					<Option key={type} value={type}>
+						{type}
+					</Option>
+				))}
+			</Select>
+		</Box>
+	);
 }
 
 export function MapTasks({
-  tasks,
-  isMapView,
-  setIsMapView,
+	tasks,
+	isMapView,
+	setIsMapView,
 }: {
-  tasks: TTasks;
-  isMapView: boolean;
-  setIsMapView: React.Dispatch<React.SetStateAction<boolean>>;
+	tasks: TTasks;
+	isMapView: boolean;
+	setIsMapView: React.Dispatch<React.SetStateAction<boolean>>;
 }) {
-  const [selectedTypes, setSelectedTypes] = useState<string[]>([]); // State for selected task types
+	const [selectedTypes, setSelectedTypes] = useState<string[]>([]); // State for selected task types
 
-  const tasksWithCoordinates = tasks.reduce<TaskWithCoordinates[]>(
-    (acc, task) => {
-      const latitude = task?.latitude || task.images[0]?.latitude;
-      const longitude = task?.longitude || task.images[0]?.longitude;
+	const tasksWithCoordinates = tasks.reduce<TaskWithCoordinates[]>(
+		(acc, task) => {
+			const latitude = task?.latitude || task.images[0]?.latitude;
+			const longitude = task?.longitude || task.images[0]?.longitude;
 
-      if (latitude && longitude) {
-        acc.push({
-          ...task,
-          latitude,
-          longitude,
-        });
-      }
+			if (latitude && longitude) {
+				acc.push({
+					...task,
+					latitude,
+					longitude,
+				});
+			}
 
-      return acc;
-    },
-    [],
-  );
+			return acc;
+		},
+		[],
+	);
 
-  const taskTypes = Array.from(
-    new Set(tasks.map((task) => task.__typename).filter(Boolean)),
-  );
-  // Filter tasks based on selected types
-  const filteredTasks =
-    selectedTypes.length > 0
-      ? tasksWithCoordinates.filter(
-          (task) => task.__typename && selectedTypes.includes(task.__typename),
-        ) // Assuming taskType is a property in your task object
-      : tasksWithCoordinates;
+	const taskTypes = Array.from(
+		new Set(tasks.map((task) => task.__typename).filter(Boolean)),
+	);
+	// Filter tasks based on selected types
+	const filteredTasks =
+		selectedTypes.length > 0
+			? tasksWithCoordinates.filter(
+					(task) => task.__typename && selectedTypes.includes(task.__typename),
+				) // Assuming taskType is a property in your task object
+			: tasksWithCoordinates;
 
-  return (
-    <Box>
-      <Box
-        sx={{
-          height: tableTopToolbarHeight,
-          px: 1,
-          gap: 1,
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "space-between",
-        }}
-      >
-        <Box
-          sx={{
-            display: "flex",
-            gap: 1,
-            alignItems: "center",
-          }}
-        >
-          <TaskTypeFilter
-            taskTypes={taskTypes}
-            selectedTypes={selectedTypes}
-            setSelectedTypes={setSelectedTypes}
-          />
-          <DateRange />
-        </Box>
-        <MapButton isMapView={isMapView} setIsMapView={setIsMapView} />
-      </Box>
-      <Box sx={{ position: "relative" }}>
-        <MapContainer
-          style={{
-            height: `calc(100dvh - ${headerHeight}px - ${tableTopToolbarHeight}px)`,
-          }}
-          center={position}
-          zoom={4}
-        >
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-
-          {filteredTasks.map((task) => (
-            <MapMarker key={task.id} task={task} />
-          ))}
-        </MapContainer>
-      </Box>
-    </Box>
-  );
+	return (
+		<Box>
+			<Box
+				sx={{
+					height: tableTopToolbarHeight,
+					px: 1,
+					gap: 1,
+					display: "flex",
+					alignItems: "center",
+					justifyContent: "space-between",
+				}}
+			>
+				<Box
+					sx={{
+						display: "flex",
+						gap: 1,
+						alignItems: "center",
+					}}
+				>
+					<TaskTypeFilter
+						taskTypes={taskTypes}
+						selectedTypes={selectedTypes}
+						setSelectedTypes={setSelectedTypes}
+					/>
+					<DateRange />
+				</Box>
+				<MapButton isMapView={isMapView} setIsMapView={setIsMapView} />
+			</Box>
+			<Box sx={{ position: "relative" }}>
+				<MapContainer
+					style={{
+						height: `calc(100dvh - ${headerHeight}px - ${tableTopToolbarHeight}px)`,
+					}}
+					center={position}
+					zoom={4}
+				>
+					<TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+					{filteredTasks.map((task) => (
+						<MapMarker key={task.id} task={task} />
+					))}
+				</MapContainer>
+			</Box>
+		</Box>
+	);
 }
